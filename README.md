592c682db80ce273835d1c4c62dbb72cd9cd32bf
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-09 20:38:17 -0700

    Added sample questions to js file based on README prompts

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Professional README Generator
 ## Description
-Provide a short description explaining the what, why, and how of your project. 
---
 The goal of this assignment was to create a command-line application that dynamically generates a README markdown file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer).
 
 ## Table of Contents 
@@ -13,26 +11,18 @@ The goal of this assignment was to create a command-line application that dynami
 - [Questions](#questions)
 
 ## Installation
-What are the steps required to install your project?
+These are the steps required to install my application.
 
 ## Usage
-Provide instructions and examples for use of your application.
---
 [Here](https://github.com/go-yasi/readme-md-generator) is a link to the application's GitHub repository.  
 
 ## License
-Choose a license for my application from a list of options:
---
 This file uses the [MIT Open Source License](https://choosealicense.com/).
 
 ## Contributing
-List your collaborators, if any, with links to their GitHub profiles.
---
 This application was developed by Yasi Moshtael.
 
 ## Tests
-Write tests for your application. Then provide examples on how to run them here.
---
 [Here](https://google.com) is a link to a video walkthrough demonstrating the application's functionality. 
 
 ## Questions
--- index.js
+++ index.js
@@ -3,53 +3,80 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'filename',
-        message: 'What would you like to name this file?',        
-      },
-      {
-        type: 'input',
-        name: 'filename',
-        message: 'What would you like to name this file?',   
-      },
-      {
-        type: 'input',
-        name: 'filename',
-        message: 'What would you like to name this file?',   
-      },
-      {
-        type: 'input',
-        name: 'filename',
-        message: 'What would you like to name this file?',   
-      },
-      {
-        type: 'input',
-        name: 'filename',
-        message: 'What would you like to name this file?',   
-      },
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn"t be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",        
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a brief description of your project:",   
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required to install your project?",   
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide the link to the project's GitHub repository: ",   
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "List your collaborators, if any, with links to their GitHub profiles: ",   
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Please provide a link to a video walkthrough demonstrating the application's functionality: ",   
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Choose a license for your application from the following list of options: ",   
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",   
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",   
+        },
+    ])
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+console.log(questions);
 
-// TODO: Create a function to initialize app
-function init() {}
+// fs.writeFile("README.md", (error, data) =>
+//     error ? console.error(error) : console.log(data)
+// );
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
